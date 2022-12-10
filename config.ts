ba8c509d6c8f33499dbb405cafca414c5b1f7a85
Diego Vergara Casanova <davc93@gmail.com>
2022-12-10 11:35:45 -0300

    listo

--- config.ts
+++ config.ts
@@ -1,8 +1,6 @@
-import dotenv from 'dotenv'
 
-dotenv.config()
-export const config = {
-    API_KEY:process.env.API_KEY,
-    API_SECRET:process.env.API_SECRET,
-}
+// export const config = {
+//     API_KEY:process.env.API_KEY,
+//     API_SECRET:process.env.API_SECRET,
+// }
 
--- index.ts
+++ index.ts
@@ -1,5 +1,105 @@
-import { config } from "./config";
+import dotenv from 'dotenv'
+dotenv.config()
+import {v2 as cloudinary} from 'cloudinary'
+import path from 'path';
+
+cloudinary.config({
+    secure:true
+})
+const getAssetInfo = async (publicId:string) => {
+
+    // Return colors in the response
+    const options = {
+      colors: true,
+    };
+
+    try {
+        // Get details about the asset
+        const result = await cloudinary.api.resource(publicId, options);
+        console.log(result);
+        return result.colors;
+        } catch (error) {
+        console.error(error);
+    }
+};
+const myUploadImage = async (imageName:string) => {
+
+    const imagePath = path.resolve(__dirname,'images',imageName)
+    console.log(imagePath)
+    // Use the uploaded file's name as the asset's public ID and 
+    // allow overwriting the asset with new versions
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+      folder:"test"
+      
+    };
+
+    try {
+      // Upload the image
+      const result = await cloudinary.uploader.upload(imagePath, options);
+      console.log(result);
+      return result.public_id;
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+const uploadImage = async (imagePath:string) => {
+
+    // Use the uploaded file's name as the asset's public ID and 
+    // allow overwriting the asset with new versions
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+      folder:'test'
+    };
+
+    try {
+      // Upload the image
+      const result = await cloudinary.uploader.upload(imagePath, options);
+      console.log(result);
+      return result.public_id;
+    } catch (error) {
+      console.error(error);
+    }
+};
+const createImageTag = (publicId:string, ...colors:any) => {
+
+    // Set the effect color and background color
+    const [effectColor, backgroundColor] = colors;
+
+    // Create an image tag with transformations applied to the src URL
+    let imageTag = cloudinary.image(publicId, {
+      transformation: [
+        { width: 250, height: 250, gravity: 'faces', crop: 'thumb' },
+        { radius: 'max' },
+        { effect: 'outline:10', color: effectColor },
+        { background: backgroundColor },
+      ],
+    });
+
+    return imageTag;
+};
+(async () => {
+
+    // Set the image to upload
+    const imagePath = '/home/dvcasanova/Documentos/WebDevelopment/Backends/cloudinary-api/images/desktop.jpeg';
+
+    // Upload the image
+    const publicId = await uploadImage(imagePath) as string;
+
+    // Get the colors in the image
+    const colors = await getAssetInfo(publicId);
+
+    // Create an image tag, using two of the colors in a transformation
+    const imageTag = await createImageTag(publicId, colors[0][0], colors[1][0]);
+
+    // Log the image tag to the console
+    console.log(imageTag);
+
+})();
 
 
-
-console.log(config)
